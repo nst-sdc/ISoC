c897fdec08823e363319529ff469ca53dfc40cac
Arpit Sarang <182847716+CodeMaverick-143@users.noreply.github.com>
2025-03-25 22:58:48 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,8 @@
 import { Database } from './database.types';
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://pxpjuzjghlmrvkhtlzyo.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://pxpjuzjghlmrvkhtlzyo.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export { supabase }; // Ensure supabase is exported
