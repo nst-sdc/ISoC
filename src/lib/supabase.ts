5d8deed8d4e14461b60c284c2df02d1ac889abaf
Arpit Sarang <182847716+CodeMaverick-143@users.noreply.github.com>
2025-03-25 22:52:59 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
 import { Database } from './database.types';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://pxpjuzjghlmrvkhtlzyo.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
