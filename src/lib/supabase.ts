984967b4dbcecd1faa3883c7126636fbfdd04365
Arpit Sarang <182847716+CodeMaverick-143@users.noreply.github.com>
2025-03-25 23:02:58 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,13 @@
 import { Database } from './database.types';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://pxpjuzjghlmrvkhtlzyo.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase URL or Key');
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
