5ce8acb2afbf210fc682a69720d5443fe9c7ed3b
Gourav NSS <gourav341111@gmail.com>
2025-03-31 18:06:17 +0530

    Fixed Opening Page

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,10 @@
-import { Database } from './database.types';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Missing Supabase URL or Key');
+throw new Error('Missing Supabase URL or Key');
 }
 
 const supabase = createClient(supabaseUrl, supabaseKey);
